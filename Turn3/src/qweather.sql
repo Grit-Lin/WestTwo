2ba0e7ea467f475228ac2922d34e6f3017d29938
Grit-Lin <505499947@qq.com>
2022-01-18 22:58:21 +0800

    sql

---
+++ Turn3/src/qweather.sql
@@ -0,0 +1,50 @@
+/*
+SQLyog Professional v12.09 (64 bit)
+MySQL - 5.7.30-log : Database - qweather
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`qweather` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `qweather`;
+
+/*Table structure for table `city` */
+
+DROP TABLE IF EXISTS `city`;
+
+CREATE TABLE `city` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `lat` decimal(10,0) NOT NULL,
+  `lon` decimal(10,0) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `weather` */
+
+DROP TABLE IF EXISTS `weather`;
+
+CREATE TABLE `weather` (
+  `dailyID` int(11) NOT NULL AUTO_INCREMENT,
+  `fxDate` date NOT NULL,
+  `tempMax` int(11) NOT NULL,
+  `tempMin` int(11) NOT NULL,
+  `textDay` text,
+  `id` int(11) NOT NULL,
+  PRIMARY KEY (`dailyID`),
+  KEY `id` (`id`),
+  CONSTRAINT `weather_ibfk_1` FOREIGN KEY (`id`) REFERENCES `city` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
